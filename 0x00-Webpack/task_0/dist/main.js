a22c0debece809120ea7f924c05ca397a973697a
bmn44100 <njerumurimi@gmail.com>
2023-10-23 18:38:07 +0000

    Basic setup

---
+++ 0x00-Webpack/task_0/dist/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#demo1").html("Holberton Dashboard");
+    $("#demo2").html("Dashboard data for the students");
+    $("#demo3").html("Copyright - Holberton School");
+});
