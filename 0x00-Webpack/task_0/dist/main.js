fb7a07e26b8dd4a31d89637e9e8129aa168288e2
bmn44100 <njerumurimi@gmail.com>
2023-10-23 19:05:56 +0000

    webpack

--- 0x00-Webpack/task_0/dist/main.js
+++ 0x00-Webpack/task_0/dist/main.js
@@ -1,5 +1,1 @@
-$(document).ready(function() {
-    $("#demo1").html("Holberton Dashboard");
-    $("#demo2").html("Dashboard data for the students");
-    $("#demo3").html("Copyright - Holberton School");
-});
+$(document).ready((function(){$("#demo1").html("Holberton Dashboard"),$("#demo2").html("Dashboard data for the students"),$("#demo3").html("Copyright - Holberton School")}));
