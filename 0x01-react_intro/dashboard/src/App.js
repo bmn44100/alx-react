9e1a814a205939c73e8d3ba33104cacd8798aa15
bmn44100 <njerumurimi@gmail.com>
2023-10-24 23:22:25 +0300

    Basic application

---
+++ 0x01-react_intro/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.jpg';
+import './App.css';
+
+export default function App() {
+    return (
+        <div>
+             <header class="App-header">
+                  <img src={logo} alt=''/>
+                  <h1>School dashboard</h1>
+             </header>
+
+             <body class="App-body">
+                   <p>Login to access the full dashboard</p>
+             </body>
+
+             <footer class="App-footer">
+               <p>Copyright 2020 - holberton School</p>
+             </footer>
+        </div>
+       );
+}
