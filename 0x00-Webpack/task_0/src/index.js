11885a32a5a1cf8408f34927ee664743f6feae3a
Bruno Njeru <118963661+bmn44100@users.noreply.github.com>
2023-10-29 20:42:28 +0300

    Delete 0x00-Webpack/task_0/src directory

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-
-$(document).ready(function() {
-    $("p").html("Holberton Dashboard");
-    $("p").html("Dashboard data for the students");
-    $("p").html("Copyright - Holberton School");
-});
