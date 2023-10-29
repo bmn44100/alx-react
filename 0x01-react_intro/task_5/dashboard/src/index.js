c9bdaa46ddb8d6712d36eae1f464f78f88c9977b
bmn44100 <njerumurimi@gmail.com>
2023-10-29 23:54:51 +0300

    Create a project using Webpack

---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*import './App.css'
+/*import App from './App';
+/*import Notifications from './Notifications';
+
+/*
+ReactDOM.render(
+  <React.StrictMode>
+      <div className='root-notifications'>
+        <Notifications />
+      </div>
+    <App />
+  </React.StrictMode>, 
+  document.getElementById('root')
+);
+*/
