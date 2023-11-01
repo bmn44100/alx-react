f2b59c33461e8b2bd59bdb21ad5c4aa5ec36d040
bmn44100 <njerumurimi@gmail.com>
2023-11-01 22:29:07 +0300

    Basic components

---
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Login.css'
+
+export default class App extends Component {
+    render () {
+        return (
+            <React.Fragment>
+                <div className="App">
+
+                    <body className="App-body">
+                    <p>Login to access the full dashboard</p>
+                    <label htmlFor="email" onClick={() => {
+                    // select corresponding input
+                    document.getElementById('password').focus();
+                    }}>Email</label>
+                    <input type="email" id="email" />
+                    <label htmlFor="password" onClick={() => {
+                        // select corresponding input
+                        document.getElementById('password').focus();
+                    }}>Password</label>
+                    <input type="password" id="password" />
+                    <button>OK
+                        <img src="close-icon.png" alt="" height="15px" width="15px"></img>
+                    </button>
+                    </body>
+
+                </div>
+            </React.Fragment>
+            );
+        }
+    }
