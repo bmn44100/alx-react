d7a118d47299df9d135c9ca2d2d5ac893e15b4c5
bmn44100 <njerumurimi@gmail.com>
2023-11-23 23:53:43 +0300

    Bound the actions

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import AppContext from '../App/AppContext';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+export default function Footer() {
+    const { user, logOut } = useContext(AppContext);
+
+    if (user.isLoggedIn) {
+        return (
+            <footer className="App-footer">
+              <p>{getFullYear()} - {getFooterCopy(true)}</p>
+            </footer>
+        );
+    
+    } else {
+        return (
+          <footer className="App-footer">
+            <p>Copyright {getFullYear} - {getFooterCopy(true)}</p>
+            <a href="">Contact us</a>
+          </footer>
+      );
+    }
+    
+  }
