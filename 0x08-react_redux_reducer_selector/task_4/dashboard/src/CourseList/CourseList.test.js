d8a6afdc13513adca43fcf5a81d866bfc6442601
bmn44100 <njerumurimi@gmail.com>
2023-12-05 22:33:55 +0300

    Normalizr & Immutable

---
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/CourseList/CourseList.test.js
@@ -0,0 +1,33 @@
+import { enzyme } from 'enzyme';
+import CourseList from './CourseList';
+import React from 'react';
+
+const wrapper = shallow(<CourseList />);
+
+it('renders without crashing', () => {
+  shallow(<CourseList />);
+  expect(wrapper.exists()).toBe(true);
+});
+
+it('renders 5 CourseListRow components', () => {
+    expect(wrapper.find('CourseListRow').length).toBe(5);
+});
+
+it('renders correctly if you pass an empty array or if you don’t pass the listCourses property', () => {
+    const wrapper = shallow(<CourseList />);
+    expect(wrapper.find('CourseListRow').length).toBe(5);
+    expect(wrapper.find('CourseListRow').get(0).props.textFirstCell).toEqual('Available courses');
+});
+
+it('renders correctly if you pass a list of courses', () => {
+    const wrapper = shallow(<CourseList listCourses={[
+        {id: 1, name: 'ES6', credit: 60},
+        {id: 2, name: 'Webpack', credit: 20},
+        {id: 3, name: 'React', credit: 40},
+    ]} />);
+    expect(wrapper.find('CourseListRow').length).toBe(8);
+    expect(wrapper.find('CourseListRow').get(6).props.textFirstCell).toEqual('ES6');
+    expect(wrapper.find('CourseListRow').get(6).props.textSecondCell).toEqual(60);
+    expect(wrapper.find('CourseListRow').get(7).props.textFirstCell).toEqual('Webpack');
+    expect(wrapper.find('CourseListRow').get(7).props.textSecondCell).toEqual(20);
+});
