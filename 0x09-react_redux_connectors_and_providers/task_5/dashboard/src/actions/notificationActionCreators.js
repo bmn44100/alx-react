120421518564aa2f679f67a8bc81096c4a4e9583
bmn44100 <njerumurimi@gmail.com>
2023-12-08 22:14:40 +0300

    Connect notifications: New Action Creator

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -14,3 +14,31 @@ export const setNotificationFilter = (filter) => (dispatch) => {
         filter
     });
 };
+
+
+export const setLoadingState = (loading) => {
+    return {
+        type: 'SET_LOADING_STATE',
+        loading
+    };
+}
+
+export const setNotifications = () => {
+    return {
+        type: 'FETCH_NOTIFICATIONS_SUCCESS',
+        data
+    };
+};
+
+export const fetchNotifications = () => {
+    return (dispatch) => {
+        dispatch(setLoadingState(true));
+        return fetch('./notifications.json')
+            .then((res) => res.json())
+            .then((data) => {
+                dispatch(setNotifications(data));
+            })
+            .catch((err) => {})
+            .finally(() => dispatch(setLoadingState(false)));
+    };
+};
